3594a7916ea01ce7231f29eb55f6193f6e354bd3
Victor Okpon <vikzy2rhyme@yahoo.com>
2020-06-09 14:32:01 +0100

    Fix test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,7 @@
-import app from '../src/index';
+import app from '../src/app';
 
 describe('app module', () => {
   test('it exists', async () => {
     await expect(app).toBeDefined();
   });
-
-  // test('it returns program name with SDGs', async () => {
-  //   const result = await app();
-  //   const sdgPos = (result || '').indexOf('SDG');
-  //   expect(sdgPos).toBeGreaterThanOrEqual(0);
-  // });
 });
